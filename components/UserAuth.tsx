fae5eeadbb5e91cb33a3f3177284f38973171e1b
Ivan Leo <ivanleo@Ivans-MacBook-Pro.local>
2023-04-29 19:33:09 +0800

    Fixed up main page auth issue

--- components/UserAuth.tsx
+++ components/UserAuth.tsx
@@ -1,26 +1,8 @@
 "use client";
-import React, { useState } from "react";
-import {
-  SignInButton,
-  SignUpButton,
-  useAuth,
-  useClerk,
-  useUser,
-} from "@clerk/nextjs";
+import { SignInButton } from "@clerk/nextjs";
 import { Button } from "./ui/button";
-import Link from "next/link";
 
 const UserAuth = () => {
-  const { isLoaded, isSignedIn } = useAuth();
-
-  if (isLoaded && isSignedIn) {
-    return (
-      <Link href="/dashboard">
-        <Button variant="ghost">Go To Dashboard</Button>
-      </Link>
-    );
-  }
-
   return (
     <SignInButton mode="modal">
       <Button
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,47 @@
+import { currentUser } from "@clerk/nextjs/app-beta";
 import LandingPage from "../../components/LandingPage";
+import Link from "next/link";
+import UserAuth from "../../components/UserAuth";
 
-const Home = () => {
-  return <LandingPage />;
+const Home = async () => {
+  const user = await currentUser();
+
+  return (
+    <div className="flex  flex-col items-center justify-center h-screen">
+      <div className="mx-auto max-w-2xl ">
+        <div className="hidden sm:mb-8 sm:flex sm:justify-center">
+          <div className="relative rounded-full px-3 py-1 text-sm leading-6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
+            Announcing our new beta.{" "}
+            <Link href="/waitlist" className="font-semibold text-indigo-600">
+              <span className="absolute inset-0" aria-hidden="true" />
+              Sign up today <span aria-hidden="true">&rarr;</span>
+            </Link>
+          </div>
+        </div>
+        <div className="text-center px-4">
+          <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
+            A New CRM for the digital age.
+          </h1>
+          <p className="mt-6 text-lg leading-8 text-gray-600">
+            Revolutionize your sales strategy with our new CRM - powered by GPT
+            technology for unparalleled insights and advantages
+          </p>
+          <div className="mt-10 flex items-center justify-center gap-x-6">
+            <Link href="/waitlist">
+              <p className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+                Join The Waitlist
+              </p>
+            </Link>
+            {user ? (
+              <Link href="/dashboard">Go To Dashboard</Link>
+            ) : (
+              <UserAuth />
+            )}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
